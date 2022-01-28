8a24b2d3b40c2089fbe7c051ca4c5588e4e8ec3c
tahminahesari <hesari@live.com>
2022-01-28 22:00:48 +0100

    Initial commit

---
+++ functions.js
@@ -0,0 +1,55 @@
+/* Opdracht 0: voorbeeld */
+// Schrijf een functie die de string "Goedemorgen" teruggeeft
+// ---- Verwachte uitkomst bij het aanroepen van de functie: "Goedemorgen"
+
+function morningGreeter() {
+    return "Goedemorgen";
+}
+
+const greeting = morningGreeter();
+
+console.log(greeting);
+
+/* Opdracht 1 */
+// Schrijf een functie genaamd getSchool, die de string "Novi Hogeschool" teruggeeft
+// ---- Verwachte uitkomst bij het aanroepen van de functie: "Novi Hogeschool"
+
+function getSchool() {
+    return "Novi Hogeschool";
+}
+
+const school = getSchool();
+console.log(school);
+
+
+/* Opdracht 2 */
+// Schrijf een functie genaamd compliment, die jou een complimentje geeft!
+// ---- Verwachte uitkomst bij het aanroepen van de functie: "Lekker bezig met die functies, Jan!"
+
+function giveCompliment() {
+    return "You are cute!";
+}
+
+const compliment = giveCompliment();
+console.log(compliment);
+
+/* Opdracht 3 */
+// Schrijf een functie genaamd getGrades, die een array met de cijfers 8, 8.5, 6 en 7 teruggeeft.
+// ---- Verwachte uitkomst bij het aanroepen van de functie: [8, 8.5, 6, 7]
+
+function getGrades() {
+    return [8, 8.5, 6, 7]
+
+}
+const grade = getGrades();
+console.log(grade);
+
+/* Opdracht 4 */
+// Schrijf een functie genaamd getDetails, die een object met daarin jouw eigen naam en achternaam teruggeeft.
+// ---- Verwachte uitkomst bij het aanroepen van de functie: { firstName: 'Jan', lastName: 'Janssen' }
+
+function getDetails() {
+    return {firstName: 'Tamo', lastName: 'Hesari'};
+}
+const detail = getDetails();
+console.log(detail);
